1b8a11913b1c18f2b26440806f70746648a42f1f
SoniaChauhan <sonia@cardmaker.com>
2026-02-26 02:34:39 +0530

    fix: correct OTP template ID (flifil)

--- src/services/notificationService.js
+++ src/services/notificationService.js
@@ -23,7 +23,7 @@
 import emailjs from '@emailjs/browser';
 
 const SERVICE_ID        = 'service_sicr4dp';
-const OTP_TEMPLATE_ID   = 'template_fli9nef';
+const OTP_TEMPLATE_ID   = 'template_fil9nef';
 const NOTIFY_TEMPLATE_ID= 'template_n3scahd';
 const PUBLIC_KEY        = 'veryFjXyXWyfIiXTT';
 
